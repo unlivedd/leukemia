e5ed2d940181450172efcf684298ffb51b4f2253
unlivedd <sirotkin.05@bk.ru>
2025-11-11 05:32:58 +0300

    changed run_gui

--- run_gui.bat
+++ run_gui.bat
@@ -1,6 +1,5 @@
 @echo off
-echo Установка всех необходимых библиотек...
-
+echo Обновление pip и установка всех библиотек...
 python -m pip install --upgrade pip
 
 python -m pip install --upgrade numpy pandas scikit-learn xgboost scikit-image matplotlib openpyxl tifffile
@@ -9,6 +8,14 @@ echo Библиотеки установлены / обновлены.
 
 if not exist "models" mkdir models
 
+echo  Начинаем обучение моделей...
+python src/train_models.py
+if %ERRORLEVEL% neq 0 (
+    echo  Произошла ошибка при обучении моделей!
+    pause
+    exit /b %ERRORLEVEL%
+)
+echo Модели успешно обучены и сохранены.
 
 echo Запуск GUI...
 python src/gui_app.py
